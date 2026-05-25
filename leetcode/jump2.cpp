801104037a1a540de12f7d5d7df2d3eca81ce338
Md Kaif Raza <mdkraza@iitbhilai.ac.in>
2026-05-25 12:24:57 +0530

    leetcode daily

---
+++ leetcode/jump2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+
+        vector<int> vis(n, 0);
+        queue<int> q;
+
+        q.push(0);
+        vis[0] = 1;
+
+        int farthest = 0;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            if (node == n - 1)
+                return true;
+
+            int start = max(node + minJump, farthest + 1);
+            int end = min(node + maxJump, n - 1);
+
+            for (int i = start; i <= end; i++) {
+                if (s[i] == '0' && !vis[i]) {
+                    vis[i] = 1;
+                    q.push(i);
+                }
+            }
+
+            farthest = end;
+        }
+
+        return false;
+    }
+};
